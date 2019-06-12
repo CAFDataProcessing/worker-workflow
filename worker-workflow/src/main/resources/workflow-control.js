c99907c44a19a018d7a1adad14a6d4d79746d6b8
Gaana Vishwnath <Gaana.Vishwnath@microfocus.com>
2019-06-12 13:34:39 +0100

    created function to check emptyGrammarMap

--- worker-workflow/src/main/resources/workflow-control.js
+++ worker-workflow/src/main/resources/workflow-control.js
@@ -184,6 +184,16 @@ function fieldExists(document, fieldName) {
     return document.getField(fieldName).hasValues();
 }
 
+function isNotEmptyMap(obj) {   
+    if(!obj)
+        return false;
+    var jsonObj = JSON.parse(obj);
+    if (Object.getOwnPropertyNames(jsonObj).length === 0) {
+        return false;
+    }
+    return true;
+}
+
 function fieldHasStringValue(document, fieldName, value) {
 
     var fieldValues = document.getField(fieldName).getValues();
