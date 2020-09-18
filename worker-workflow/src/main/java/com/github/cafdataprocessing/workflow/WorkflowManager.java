c32ea167dd79af210aa2e5314f8960e04d3da4b1
Dom Gibson <dominic.gibson@microfocus.com>
2020-09-18 15:38:22 +0100

    Wrap the function def in String.raw call

--- worker-workflow/src/main/java/com/github/cafdataprocessing/workflow/WorkflowManager.java
+++ worker-workflow/src/main/java/com/github/cafdataprocessing/workflow/WorkflowManager.java
@@ -114,7 +114,7 @@ public class WorkflowManager {
                 try {
                     final String addFailures = Resources.toString(Resources.getResource("add-failures.js"),
                             StandardCharsets.UTF_8).replaceAll("`", "\\`");
-                    stringBuilder.append("thisScriptObject = `\n").append(addFailures);
+                    stringBuilder.append("thisScriptObject = String.raw`\n").append(addFailures);
                     if(contextScriptFileContent != null) {
                         stringBuilder.append(contextScriptFileContent);
                     }
