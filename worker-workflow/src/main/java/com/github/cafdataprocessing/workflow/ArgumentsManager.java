b2d28c7758c3b43999b96b4fdac5176cbaa2bd07
rorytorneymf <rory.torney@microfocus.com>
2021-09-01 12:21:19 +0100

    SCMOD-6849: Further updates (working, but WIP)

--- worker-workflow/src/main/java/com/github/cafdataprocessing/workflow/ArgumentsManager.java
+++ worker-workflow/src/main/java/com/github/cafdataprocessing/workflow/ArgumentsManager.java
@@ -16,6 +16,7 @@
 package com.github.cafdataprocessing.workflow;
 
 import com.github.cafdataprocessing.workflow.model.ArgumentDefinition;
+import com.google.common.base.Joiner;
 import com.google.common.base.Strings;
 import com.google.gson.Gson;
 import com.hpe.caf.worker.document.exceptions.DocumentWorkerTransientException;
@@ -27,7 +28,9 @@ import com.squareup.okhttp.logging.HttpLoggingInterceptor;
 import com.squareup.okhttp.logging.HttpLoggingInterceptor.Level;
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.nio.file.Files;
+import java.time.Instant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -35,6 +38,7 @@ import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 
 public class ArgumentsManager {
@@ -43,6 +47,7 @@ public class ArgumentsManager {
     private static final int SETTINGS_SERVICE_CACHE_SIZE_BYTES = 10 * 1024 * 1024; // 10 MiB
     private static final String SETTINGS_SERVICE_CACHE_TEMP_DIRECTORY_PREFIX = "settings-service-http-cache";
     private static final Interceptor FORCE_CACHE_REFRESH_CACHE_CONTROL_INTERCEPTOR = new ForceCacheRefreshCacheControlInterceptor();
+    private static final Map<String, Long> SETTINGS_SERVICE_LAST_ACCESS_TIME_MAP = new HashMap<>();
 
     private final Gson gson = new Gson();
     private final SettingsApi settingsApi;
@@ -67,6 +72,7 @@ public class ArgumentsManager {
         final Cache cache = new Cache(settingsServiceCacheDirectory, SETTINGS_SERVICE_CACHE_SIZE_BYTES);
         okHttpClient.setCache(cache);
         okHttpClient.networkInterceptors().add(getCacheControlInterceptor());
+        okHttpClient.networkInterceptors().add(new RecordLastAccessTimeInterceptor());
         HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
         logging.setLevel(Level.BODY);
         okHttpClient.interceptors().add(logging);
@@ -79,17 +85,23 @@ public class ArgumentsManager {
 
     private Interceptor getCacheControlInterceptor() {
         return chain -> {
-            final Response response = chain.proceed(chain.request());
-            final CacheControl cacheControl = new CacheControl.Builder()
+            final Request request = chain.request();
+            if (shouldCacheSettingsServiceResponse(request)) {
+                final Response response = chain.proceed(request);
+                final CacheControl cacheControl = new CacheControl.Builder()
                     .maxAge(5, TimeUnit.MINUTES)
                     .build();
 
-            return response.newBuilder()
+                return response.newBuilder()
                     .header("Cache-Control", cacheControl.toString())
                     .build();
+            } else {
+                return chain.proceed(request); 
+            }
         };
     }
 
+    // Important that this is added as an application interceptor (as opposed to a network interceptor), otherwise it has no affect.
     private final static class ForceCacheRefreshCacheControlInterceptor implements Interceptor
     {
         @Override
@@ -102,6 +114,46 @@ public class ArgumentsManager {
             final Request originalRequest = chain.request();
             final Request forceCacheRefreshRequest = originalRequest.newBuilder().cacheControl(cacheControl).build();
             return chain.proceed(forceCacheRefreshRequest);
+        }
+    }
+
+    // Important that this is added as a network interceptor (as opposed to an application interceptor), to ensure it won't be invoked
+    // for cached responses.
+    private final static class RecordLastAccessTimeInterceptor implements Interceptor
+    {
+        @Override
+        public Response intercept(Interceptor.Chain chain) throws IOException
+        {
+            final Request request = chain.request();
+            if (shouldCacheSettingsServiceResponse(request)) {
+                LOG.warn("RORY updating map. Before: " + Arrays.toString(SETTINGS_SERVICE_LAST_ACCESS_TIME_MAP.entrySet().toArray()));
+                //   request.uri()
+                LOG.warn("RORY URL PATH " + request.url().getPath());
+                LOG.warn("RORY uri PATH " + request.uri().getPath());
+                LOG.warn("RORY uri raw path " + request.uri().getRawPath());
+                LOG.warn("RORY httpurl encoded path " + request.httpUrl().encodedPath());
+
+                /**
+                 * RORY URL PATH /settings/ee.grammarmap/resolved RORY uri PATH /settings/ee.grammarmap/resolved RORY uri raw path
+                 * /settings/ee.grammarmap/resolved RORY httpurl encoded path /settings/ee.grammarmap/resolved
+                 *
+                 */
+                LOG.warn("RORY URL query " + request.url().getQuery());
+                LOG.warn("RORY uri query " + request.uri().getQuery());
+                LOG.warn("RORY uri raw query " + request.uri().getRawQuery());
+                LOG.warn("RORY httpurl encoded query " + request.httpUrl().encodedQuery());
+
+                LOG.warn("RORY URL toString " + request.url().toString());
+                LOG.warn("RORY urlString " + request.urlString());
+
+                final URI uri = request.uri();
+                final String key = String.format("%s?%s", uri.getRawPath(), uri.getRawQuery());
+                SETTINGS_SERVICE_LAST_ACCESS_TIME_MAP.put(key, Instant.now().toEpochMilli());
+                LOG.warn("RORY updated map: After: " + Arrays.toString(SETTINGS_SERVICE_LAST_ACCESS_TIME_MAP.entrySet().toArray()));
+                return chain.proceed(request);
+            } else {
+                return chain.proceed(request);
+            }
         }
     }
 
@@ -222,15 +274,17 @@ public class ArgumentsManager {
 
         final ResolvedSetting resolvedSetting;
         try {
-            if (settingsServiceLastUpdateTimeMillisOpt.isPresent()) {
-                try {
-                    okHttpClient.interceptors().add(FORCE_CACHE_REFRESH_CACHE_CONTROL_INTERCEPTOR);
-                    resolvedSetting = settingsApi.getResolvedSetting(name, String.join(",", scopes), String.join(",", priorities));
-                } finally {
-                    okHttpClient.interceptors().removeIf(ForceCacheRefreshCacheControlInterceptor.class::isInstance);
-                }
+            if (shouldForceSettingsServiceCacheRefresh(name, scopes, priorities, settingsServiceLastUpdateTimeMillisOpt)) {
+                 LOG.warn(String.format("RORY number of interceptors before forcing cache refresh" + okHttpClient.interceptors().size()));
+                 LOG.warn(String.format("RORY forcing settings service cache refresh."));
+                    resolvedSetting = forceSettingsServiceCacheRefresh(
+                        () -> settingsApi.getResolvedSetting(name, String.join(",", scopes), String.join(",", priorities)));
+                LOG.warn(String.format("RORY number of interceptors after forcing cache refresh" + okHttpClient.interceptors().size()));
             } else {
+                LOG.warn(String.format("RORY NOT forcing settings service cache refresh."));
+                LOG.warn(String.format("RORY number of interceptors before NOT forcing cache refresh" + okHttpClient.interceptors().size()));
                 resolvedSetting = settingsApi.getResolvedSetting(name, String.join(",", scopes), String.join(",", priorities));
+                LOG.warn(String.format("RORY number of interceptors after NOT forcing cache refresh" + okHttpClient.interceptors().size()));
             }
             
         } catch (final ApiException e) {
@@ -249,15 +303,81 @@ public class ArgumentsManager {
     @SuppressWarnings("unused")
     public void checkHealth() {
         try {
-            okHttpClient.interceptors().add(FORCE_CACHE_REFRESH_CACHE_CONTROL_INTERCEPTOR);
             settingsApi.getSetting("healthcheck");
         }
         catch (final ApiException ex){
             if(ex.getCode()!=404){
                 throw new RuntimeException(ex.getMessage(), ex);
             }
+        }
+    }
+
+    private static boolean shouldCacheSettingsServiceResponse(final Request request)
+    {
+        return !request.urlString().contains("healthcheck");
+    }
+
+    private static boolean shouldForceSettingsServiceCacheRefresh(
+        final String settingName,
+        final List<String> scopes,
+        final List<String> priorities,
+        final Optional<Long> settingsServiceLastUpdateTimeMillisOpt)
+    {
+        if (!settingsServiceLastUpdateTimeMillisOpt.isPresent()) {
+            return false;
+        }
+        final Long settingsServiceLastUpdateTimeMillis = settingsServiceLastUpdateTimeMillisOpt.get();
+        final String scopesUrlQueryParam = (scopes != null && !scopes.isEmpty()) 
+            ? "scopes=" + String.join(",", scopes)
+            : null;
+        final String prioritiesUrlQueryParam = (priorities != null && !priorities.isEmpty())
+            ? "priorities=" + String.join(",", priorities)
+            : null;
+        final String urlQueryParams = Joiner.on("&").skipNulls().join(
+            scopesUrlQueryParam,
+            prioritiesUrlQueryParam
+        );
+            
+            
+        LOG.warn("RORY in new method urlQueryParams " + urlQueryParams);
+        
+        final String key = urlQueryParams != null ? 
+            String.format("/settings/%s/resolved?%s", settingName, urlQueryParams) :
+            String.format("/settings/%s/resolved", settingName);
+        LOG.warn("RORY in new method key " + key);
+        
+        final Long lastAccessTime = SETTINGS_SERVICE_LAST_ACCESS_TIME_MAP.get(key);
+        if (lastAccessTime == null) {
+            LOG.warn("RORY in new map does not contain key " + key);
+            return false;
+        }
+        LOG.warn("RORY in new map does contains key " + key + " + with value " + lastAccessTime);
+        if (settingsServiceLastUpdateTimeMillisOpt.get() > lastAccessTime) {
+            LOG.warn("RORY returning true " + settingsServiceLastUpdateTimeMillis + " > " + lastAccessTime);
+            return true;
+        }  else {
+            LOG.warn("RORY returning false " + settingsServiceLastUpdateTimeMillis + " > " + lastAccessTime);
+            return false;
+        }
+        
+
+//            .map(s -> "?" + s)
+//            .orElse("");
+    }
+
+    private ResolvedSetting forceSettingsServiceCacheRefresh(final SettingsServiceApiCall settingsServiceApiCall) throws ApiException
+    {
+        try {
+            okHttpClient.interceptors().add(FORCE_CACHE_REFRESH_CACHE_CONTROL_INTERCEPTOR);
+            return settingsServiceApiCall.call();
         } finally {
             okHttpClient.interceptors().removeIf(ForceCacheRefreshCacheControlInterceptor.class::isInstance);
         }
+    }
+
+    @FunctionalInterface
+    private interface SettingsServiceApiCall
+    {
+        public ResolvedSetting call() throws ApiException;
     }
 }
