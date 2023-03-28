2b3ecb6da23b803bd64331bac1a7b3fda8addc65
Rory <rory.torney@microfocus.com>
2023-03-28 09:28:46 +0100

    632159: Add WMP healthcheck

--- release-notes-6.1.0.md
+++ release-notes-6.1.0.md
@@ -4,5 +4,7 @@
 ${version-number}
 
 #### New Features
+- US632159: This worker's healthcheck will now check the status of the Rabbit MQ Management API whenever the `CAF_WMP_ENABLED` 
+  environment variable is true.
 
 #### Known Issues
--- worker-workflow/src/main/java/com/github/cafdataprocessing/workflow/WorkflowWorker.java
+++ worker-workflow/src/main/java/com/github/cafdataprocessing/workflow/WorkflowWorker.java
@@ -16,6 +16,7 @@
 package com.github.cafdataprocessing.workflow;
 
 import com.github.cafdataprocessing.workflow.model.Workflow;
+import com.github.workerframework.workermessageprioritization.rerouting.MessageRouterSingleton;
 import com.google.common.base.Strings;
 import com.hpe.caf.api.ConfigurationException;
 import com.hpe.caf.worker.document.exceptions.DocumentWorkerTransientException;
@@ -94,6 +95,8 @@ public final class WorkflowWorker implements DocumentWorker
             LOG.error("Problem encountered when contacting Settings Service to check health: ", e);
             healthMonitor.reportUnhealthy("Settings Service communication is unhealthy: " + e.getMessage());
         }
+
+        MessageRouterSingleton.checkHealth(healthMonitor);
     }
 
     /**
