d29c61ede13a71ea5ae8540414eb4c660bd3d07f
Raghavendran Gopalakrishnan <rgopalakris3@opentext.com>
2025-01-10 13:28:39 +0530

    US1002065: Update copyrights statements to 2025 (#213)
    https://internal.almoctane.com/ui/entity-navigation?p=131002/6001&entityType=work_item&id=1002065

--- worker-workflow/src/main/resources/workflow-control.js
+++ worker-workflow/src/main/resources/workflow-control.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2024 Open Text.
+ * Copyright 2017-2025 Open Text.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/* global Java, thisScript */
 var UnsupportedOperationException = Java.type("java.lang.UnsupportedOperationException");
 var RuntimeException = Java.type("java.lang.RuntimeException");
 var ArrayList = Java.type("java.util.ArrayList");
