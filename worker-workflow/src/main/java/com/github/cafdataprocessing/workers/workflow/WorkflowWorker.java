8ef59f281a1920a25837c9b511b75f46a7c032e7
Jonathan McCoubrey <jmccoubrey@opentext.com>
2025-02-06 16:19:52 +0000

    Revert "Get correlation ID from MDC"
    This reverts commit 6fc942deacc44cab29305d4a91efa404262c1e82.

--- worker-workflow/src/main/java/com/github/cafdataprocessing/workers/workflow/WorkflowWorker.java
+++ worker-workflow/src/main/java/com/github/cafdataprocessing/workers/workflow/WorkflowWorker.java
@@ -197,13 +197,11 @@ public final class WorkflowWorker implements DocumentWorker
     
     private static String getOrCreateCorrelationId(final Task task)
     {
-        String correlationId = MDC.get("correlationId");
-        if (correlationId == null) {
-            correlationId = UUID.randomUUID().toString();
-            LOG.error("correlationId not set in MDC context. Generating new correlationId: {}", correlationId);
-        }
+        final String correlationId = task.getCustomData(CORRELATION_ID_KEY);
 
-        return correlationId;
+        return (correlationId == null)
+            ? UUID.randomUUID().toString()
+            : correlationId;
     }
 
     private static Optional<Long> getSettingsServiceLastUpdateTimeMillis(final Document document) throws NumberFormatException
